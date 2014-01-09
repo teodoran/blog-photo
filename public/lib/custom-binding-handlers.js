e5df2481e09a1fbadf20ba97f851a642b5a3491c
Teodor Ande Elstad <teodoran@stud.ntnu.no>
2014-01-09 12:35:05 +0100

    Added syntax highligthing by using highligth.js in the custom binding handler

--- public/lib/custom-binding-handlers.js
+++ public/lib/custom-binding-handlers.js
@@ -1,6 +1,14 @@
-/*global ko, $, marked*/
+/*global ko, $, marked, highlightAuto*/
 
 ko.bindingHandlers.markdown = {
+    init: function () {
+        "use strict";
+        marked.setOptions({
+            highlight: function (code) {
+                return hljs.highlightAuto(code).value;
+            }
+        });
+    },
     update: function(element, valueAccessor) {
         "use strict";
         var valueUnwrapped = ko.unwrap(valueAccessor());
